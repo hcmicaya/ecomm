1d48af04234fc093fc20dda95ef1bfd0d060425d
Hans Micaya <hansmicaya@yahoo.com>
2021-11-01 22:11:03 -0400

    Second

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const usersRepo = require('./repositories/users');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
+
+app.use(express.static('public')); // public folder can be accessed
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['asdczxc']
+  })
+);
 
-app.get('/', (req, res) => {
-  res.send(`
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <input name="passwordConfirmation" placeholder="password confirmation" />
-        <button>Sign Up</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post('/', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send('Email in use');
-  }
-
-  if (password !== passwordConfirmation) {
-    return res.send('Password must match');
-  }
-
-  res.send('Account created!!!');
-});
+app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log('Listening');
