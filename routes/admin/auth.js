99f4484f5c43c8303239698001fe6909a49f6ba8
Hans Micaya <hcmicaya@gmail.com>
2022-02-13 18:20:23 -0500

    test

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
 
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -37,7 +38,7 @@ router.post(
         
         req.session.userId = user.id; // userId can be named anything
 
-        res.send('Account created!!!');
+        res.redirect('/admin/products');
     }
 );
 
@@ -56,19 +57,15 @@ router.post(
     requireEmailExists,
     requireValidPasswordForUser
     ],
+    handleErrors(signinTemplate),
     async (req, res) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            return res.send(signinTemplate({ errors }));
-        }
         const { email } = req.body;
 
         const user = await usersRepo.getOneBy({ email });
        
         req.session.userId = user.id;
 
-        res.send('You are signed')
+        res.redirect('/admin/products');
     }
 );
 
--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -4,11 +4,13 @@ const usersRepo = require('../../repositories/users');
 module.exports = {
     requireTitle: check('title')
         .trim()
-        .isLength({ min: 5, max: 40 }),
+        .isLength({ min: 5, max: 40 })
+        .withMessage('Must be between 5 and 40 characters'),
     requirePrice: check('price')
         .trim()
         .toFloat()
-        .isFloat({ min: 1 }),
+        .isFloat({ min: 1 })
+        .withMessage('Must be a number greater than 1'),
     requireEmail: check('email')
         .trim()
         .normalizeEmail()
@@ -32,6 +34,7 @@ module.exports = {
             if (passwordConfirmation !== req.body.password) {
                 throw new Error('Passwords must match');
             }
+            return true;// to fix bug
         }),
     requireEmailExists: check('email')
     .trim()
